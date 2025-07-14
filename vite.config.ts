83947578262276d76cd4fd7120bc075d8ffbb3ea
AGFH5300 <dxb.avg@gmail.com>
2025-07-15 01:00:57 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  root: path.resolve(import.meta.dirname, "client"),
+
+  publicDir: path.resolve(import.meta.dirname, "client", "public"),
+  
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -23,7 +27,7 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
