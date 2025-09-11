d4727553a08ac23cce3cc0484fd843925b5ce42f
AGFH5300 <dxb.avg@gmail.com>
2025-09-11 08:32:30 +0400

    Update personalData.ts

--- client/src/data/personalData.ts
+++ client/src/data/personalData.ts
@@ -190,17 +190,17 @@ export const personalData = {
           url: "https://i.postimg.cc/8CnbSDyh/diatech.jpg",
           downloadUrl: "https://i.postimg.cc/69BbCpn1/diatech.jpg?dl=1",
           caption: "DIATECH Winner Certificate"
-        }
+        },
         {
           url: "https://i.postimg.cc/j5BGj9R8/diatech-robot.jpg",
           downloadUrl: "https://i.postimg.cc/4f3ZWkqT/diatech-robot.jpg?dl=1",
           caption: "DIATECH Winners Photo"
-        }
+        },
         {
           url: "https://i.postimg.cc/s26kkxqC/diatech-norobot.png",
           downloadUrl: "https://i.postimg.cc/x0n9bbP8/diatech-norobot.png?dl=1",
           caption: "DIATECH Winners Photo"
-        }
+        },
       ],
     },
   ],
